c2434ff37973842c1f2e9fabe848cf5a945d59f8
David Brieden <davidbrieden@aol.com>
2018-03-11 13:55:30 +0100

    styling

--- gulpfile.js
+++ gulpfile.js
@@ -9,34 +9,34 @@ var source      = require('vinyl-source-stream');
 var webserver   = require('./src/server/webserver.js');
 
 gulp.task('browserify', function(callback) {
-    var bundler = browserify({
-        // Required watchify args
-        cache: {}, packageCache: {}, fullPaths: false,
-        // Specify the entry point of your app
-        entries: 'src/client/scripts/main.js',
-        debug:true,
-        plugin: [watchify],
-        extensions: ['js']
-    });
-    bundler.transform(babelify.configure({presets: ["es2015"]}));
-    bundler.transform(vueify);
+  var bundler = browserify({
+    // Required watchify args
+    cache: {}, packageCache: {}, fullPaths: false,
+    // Specify the entry point of your app
+    entries: 'src/client/scripts/main.js',
+    debug:true,
+    plugin: [watchify],
+    extensions: ['js']
+  });
+  bundler.transform(babelify.configure({presets: ["es2015"]}));
+  bundler.transform(vueify);
 
-    var bundle = function(){
-        bundler
-            .bundle()
-            .on('error', function(err){gutil.log(err.message)})
-            .pipe(source('app.js'))
-            // Specify the output destination
-            .pipe(gulp.dest('src/client/wwwroot'));
-    };
+  var bundle = function(){
+      bundler
+        .bundle()
+        .on('error', function(err){gutil.log(err.message)})
+        .pipe(source('app.js'))
+        // Specify the output destination
+        .pipe(gulp.dest('src/client/wwwroot'));
+  };
 
-    bundler.on('update', bundle);
-    bundle();
+  bundler.on('update', bundle);
+  bundle();
 
 });
 
 gulp.task('webserver', function() {
-    webserver.start();
+  webserver.start();
 });
 
 gulp.task('default', ['browserify', 'webserver']);
--- src/server/webserver.js
+++ src/server/webserver.js
@@ -3,15 +3,13 @@ var app         = express();
 var http        = require('http').Server(app);
 
 var start = function() {
-    
-    app.use(express.static('src/client/wwwroot'));
+  app.use(express.static('src/client/wwwroot'));
 
-    http.listen(process.env.PORT || 3000, function(){
-        console.log('listening on *:3000');
-    });
-
+  http.listen(process.env.PORT || 3000, function(){
+    console.log('listening on *:3000');
+  });
 };
 
 module.exports = {
-    start: start
+  start: start
 }
